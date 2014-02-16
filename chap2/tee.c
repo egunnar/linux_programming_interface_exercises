5e46061478bec125c5100dee3de375e9cc2aafa7
Erik Gartz <eegunnar@yahoo.com>
2014-02-16 15:29:16 -0500

    seems to work

--- chap2/tee.c
+++ chap2/tee.c
@@ -2,13 +2,35 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
 
+int BUFFER_SIZE = 1000;
+
+
+void exitError(char * additional_message){
+	fprintf(stderr, additional_message);
+	fprintf(stderr, strerror(errno));
+	exit(1);
+}
+
+void usage(){
+	fprintf(stderr, "Usage: tee [-a] [FILE]\n");
+	exit(1);
+}
 
 int main(int argc, char *argv[]){
 
 	bool do_append = false;
 	int c;	
 	char * file_name = "";
+	char *buff = malloc(BUFFER_SIZE);
+	int bytes_read;
+	if (buff == NULL){
+		fprintf(stderr, "malloc failed");
+		exit(1);
+	}
 
 	while ((c = getopt(argc, argv, "a")) != -1 ){
 		if (c == 'a'){
@@ -17,17 +39,35 @@ int main(int argc, char *argv[]){
 			exit(1);
 		}
 	}
-	fprintf(stderr, "optind:%d\n", optind);
+
+	if (do_append && (argc != 3))
+		usage();
+	if ((!do_append) && (argc != 2))
+		usage();
 
 	file_name = argv[optind];
 
+	int open_flag = O_WRONLY | O_CREAT;
 	if (do_append){
-		fprintf(stderr, "do_append\n");
-	}else{
-		fprintf(stderr, "NO do_append\n");
+		open_flag |= O_APPEND;
 	}
-	fprintf(stderr, "file_name:%s\n", file_name);
+	// rw-rw-rw- 
+	mode_t permission_mode = S_IROTH | S_IWOTH | S_IRUSR | S_IWUSR | 
+		S_IRGRP | S_IWGRP;
 
-
+	int fd_tee_file = open(file_name, open_flag, permission_mode);
+	
+	while(1){	
+		bytes_read = read(STDIN_FILENO, buff, BUFFER_SIZE);
+		if (bytes_read == -1){
+		}
+		fprintf(stderr, "bytes read:%d\n", bytes_read);
+		if (bytes_read <= 0){
+			break;
+		}
+		write(STDOUT_FILENO, buff, bytes_read);
+		write(fd_tee_file, buff, bytes_read);
+	}	
+	close(fd_tee_file);
 	return 0;
 }
